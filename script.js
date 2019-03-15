64638b77defecbae41fc50020ffe99399550706a
hikeislife <hikeislife@users.noreply.github.com>
2019-03-15 12:29:33 +0100

    files

--- script.js
+++ script.js
@@ -9,8 +9,70 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+//  //    if (window.pageYOffset > 1560) {
+// 	// 	document.querySelector(".hr-black-double").style.width = "80%";
+// 	// 	document.querySelector(".title-usluge").style.fontSize = "77px";
+// 	// 	document.querySelector(".title-usluge").style.transition = "1.5s";
+// 	// }
+// 	// else {
+// 	// 	document.querySelector(".hr-black-double").style.width = "20%";
+// 	// 	document.querySelector(".hr-black-double").style.transition = "1.5s";
+// 	// 	document.querySelector(".title-usluge").style.fontSize = "55px";
+// 	// 	document.querySelector(".title-usluge").style.transition = "1.5s";
+// 	// }
+// }
 
 //  active link on click
 $(document).on('click', 'ul li', function() {
   $(this).addClass('active').siblings().removeClass('active');
 });
+
+
+///       snippets      ///
+// var $ = (el) => document.querySelector(el);
+// var log = (param) => console.log(param);
+///       *******      ///
+
+
+
+
+
+
+
+	
+	
+// function apoeni(suma) {
+
+// 	var arr = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10]
+
+// 	for (var i = 0; i < arr.length; i++) {
+		
+// 		apoen = (suma - (suma % arr[i])) / arr[i]
+// 		suma = suma % arr[i]
+
+// 		if (apoen > 0) {
+// 			console.log(apoen + " apoena od " + arr[i] + "din")
+// 		}
+
+// 		if (suma < 10) {
+// 			console.log("ostatak je " + suma + "din")
+// 		}
+// 	}
+// }
+
+// apoeni(10678)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
